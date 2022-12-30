c6907f0b3f9cdb270e2c9412bbc6966295a4193f
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-30 09:31:20 +0530

    index

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn1=document.getElementById("btn1");
+let btn2=document.getElementById("btn2");
+btn1.addEventListener("click",myFunct);
+btn2.addEventListener("click",myFunct1);
+
+function myFunct(){
+  let add=document.getElementById("number").value;
+  add++;
+  document.getElementById("number").value=add;
+}
+function myFunct1(){
+  let add=document.getElementById("number").value;
+  add--;
+  document.getElementById("number").value=add;
+}
+
